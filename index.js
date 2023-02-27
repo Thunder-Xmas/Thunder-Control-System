a1942be868ec8f28b4baf2d20df19504b3be7db2
Thunder-Xmas <124792396+Thunder-Xmas@users.noreply.github.com>
2023-02-27 12:30:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function audio(){
     document.getElementById('btn_audio').currentTime = 0; //連続クリックに対応
     document.getElementById('btn_audio').play(); //クリックしたら音を再生
 }
+
+function audio2(){
+    document.getElementById('btn_audio2').currentTime = 0; //連続クリックに対応
+    document.getElementById('btn_audio2').play(); //クリックしたら音を再生
+}
